193d863ba972c5f3fa6f46e6a4c385661b51fe92
Samuel Hellawell <sshellawell@gmail.com>
2022-05-10 19:25:17 +0100

    design update

--- src/views/scan-qr.js
+++ src/views/scan-qr.js
@@ -3,14 +3,14 @@ import QRCode from 'qrcode';
 export default async function getPageHTML(query, qrUrl) {
   const qrData = await QRCode.toDataURL(qrUrl);
   return `
-    <html>
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="stylesheet" href="/globals.css"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Nunito Sans"/>
         <title>
-          Dock DID Auth
+          Dock Auth
         </title>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="/refresh.js" referrerpolicy="no-referrer"></script>
@@ -19,15 +19,34 @@ export default async function getPageHTML(query, qrUrl) {
         <div class="content-wrapper">
           <div class="main" id="sign-in">
             <h1>
-              Sign in with your DID
+              Sign in with your Dock Wallet
             </h1>
             <p>
-              Scan the QR code or click the deep link to sign in with your DID using the Dock Wallet.<br />
-              This page should refresh automatically once confirmed by your app, if it doesnt refresh <a href="javascript:window.location.href=window.location.href">click here</a>.
+              Scan the QR code or click the button to sign in with your DID using the Dock Wallet.
             </p>
-            <img src="${qrData}" />
-            <br />
-            <a class="submit-btn" href="${qrUrl}">Clicky to sign in</a>
+            <div class="qr-wrapper">
+              <img src="${qrData}" alt="qr-code" />
+            </div>
+            <a class="submit-btn" href="${qrUrl}">
+              Sign in with Dock Wallet
+            </a>
+            <div class="get-wallet-prompt">
+              <div class="get-wallet-prompt-logo">
+                <img src="/wallet-icon.svg" />
+              </div>
+              <div class="get-wallet-prompt-content">
+                <h2>Get Dock Wallet</h2>
+                <p>Available both on iOS and Android</p>
+                <div class="get-wallet-buttons">
+                  <a href="#">
+                    <img src="/app-store.svg" alt="app-store-btn" />
+                  </a>
+                  <a href="#">
+                    <img src="/gplay.svg" alt="gplay-store-btn" />
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="main" id="redirecting" style="display: none">
             <h1>
