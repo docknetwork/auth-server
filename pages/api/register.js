447ee1fa0d8618dbc8cf9935e3fb56dab220612c
Samuel Hellawell <sshellawell@gmail.com>
2022-06-09 16:12:22 +0100

    register test, url validation
    Signed-off-by: Samuel Hellawell <sshellawell@gmail.com>

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,4 +1,5 @@
 import { encodeClientId, createClientSecret } from '../../src/utils/client-crypto';
+import isValidHttpUrl from '../../src/utils/valid-url';
 
 export default async (req, res) => {
   if (req.method !== 'POST') {
@@ -8,37 +9,35 @@ export default async (req, res) => {
     return;
   }
 
-  const { name = 'Unnamed App', redirect_uris, website } = req.body;
+  const { name, redirect_uris, website } = req.body;
 
   // Ensure required parameters exist
   if (!redirect_uris || !name || !website || !Array.isArray(redirect_uris)) {
-    res.status(400).json({
-      error: 'Parameters are required: redirect_uris (array of urls), name (string), website (url)',
-    });
+    res.status(400).send('Parameters are required: redirect_uris (array of urls), name (string), website (url)');
     return;
   }
 
   if (redirect_uris.length > 1) {
-    res.status(400).json({
-      error: 'Only one redirect_uri is supported at the moment',
-    });
+    res.status(400).send('Only one redirect_uri is supported at the moment');
     return;
   }
 
-  try {
-    const clientId = encodeClientId(req.body);
-    const clientSecret = createClientSecret(clientId, redirect_uris);
-
-    res.json({
-      client_id: clientId,
-      client_secret: clientSecret,
-    });
-  } catch (e) {
-    console.error(e);
-    res.status(400).json({
-      error: `Unable to generate credentials: ${e.message}`,
-    });
+  if (!isValidHttpUrl(website)) {
+    res.status(400).send('Website must be a valid HTTP/HTTPS URL');
+    return;
   }
+
+  if (!isValidHttpUrl(redirect_uris[0])) {
+    res.status(400).send('redirect_uri must be a valid HTTP/HTTPS URL');
+    return;
+  }
+
+  const clientId = encodeClientId(req.body);
+  const clientSecret = createClientSecret(clientId, redirect_uris);
+  res.json({
+    client_id: clientId,
+    client_secret: clientSecret,
+  });
 };
 
 export const config = {
---
+++ tests/integration/pages/register.test.js
@@ -0,0 +1,112 @@
+import memjs from 'memjs';
+import { createMocks } from 'node-mocks-http';
+
+import handleRegister from '../../../pages/api/register';
+import { createAuthRequest, submitCredential, getAccessToken } from './helpers';
+
+jest.mock('memjs');
+
+const registerOptions = {
+  redirect_uris: ['http://localhost:3000/login/callback'],
+  name: 'Test App',
+  website: 'http://localhost:3000',
+};
+
+describe('API Route - /register', () => {
+  let authParams;
+
+  afterEach(() => {
+    memjs.mockReset();
+  });
+
+  beforeEach(async () => {
+    // await createAuthRequest();
+    // await submitCredential();
+    // authParams = await createAuthRequest();
+  });
+
+  test('can register a new client to get id and secret', async () => {
+    const { req, res } = createMocks({
+      method: 'POST',
+      body: registerOptions,
+    });
+
+    await handleRegister(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(res._isJSON()).toBe(true);
+
+    const data = JSON.parse(res._getData());
+    expect(data.client_id).toBeDefined();
+    expect(data.client_secret).toBeDefined();
+  });
+
+  test('only POST method is supported', async () => {
+    const { req, res } = createMocks({
+      method: 'GET',
+    });
+    await handleRegister(req, res);
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  test('can not register with no parameters', async () => {
+    const { req, res } = createMocks({
+      method: 'POST',
+      body: {
+        redirect_uris: [],
+        name: '',
+        website: '',
+      },
+    });
+    await handleRegister(req, res);
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  test('can not register with invalid redirect_uris (not an array)', async () => {
+    const { req, res } = createMocks({
+      method: 'POST',
+      body: {
+        ...registerOptions,
+        redirect_uris: 'astring',
+      },
+    });
+    await handleRegister(req, res);
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  test('can not register with invalid redirect_uris (not a uri)', async () => {
+    const { req, res } = createMocks({
+      method: 'POST',
+      body: {
+        ...registerOptions,
+        redirect_uris: ['astring'],
+      },
+    });
+    await handleRegister(req, res);
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  test('can not register with invalid redirect_uris (multiple)', async () => {
+    const { req, res } = createMocks({
+      method: 'POST',
+      body: {
+        ...registerOptions,
+        redirect_uris: ['http://localhost', 'https://dock.io'],
+      },
+    });
+    await handleRegister(req, res);
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  test('can not register with invalid website', async () => {
+    const { req, res } = createMocks({
+      method: 'POST',
+      body: {
+        ...registerOptions,
+        website: 'notauri',
+      },
+    });
+    await handleRegister(req, res);
+    expect(res._getStatusCode()).toBe(400);
+  });
+});
