8f5cf1899042b9e90cc0688364351cdde901e400
Sam Hellawell <sshellawell@gmail.com>
2025-01-22 14:43:06 +0000

    format

--- src/config.js
+++ src/config.js
@@ -7,7 +7,8 @@ export const GPLAY_STORE_URI = 'https://play.google.com/store/apps/details?id=co
 export const API_KEY = process.env.API_KEY;
 export const API_KEY_TESTNET = process.env.API_KEY_TESTNET;
 
-export const DOCK_API_VERIFY_URL = process.env.DOCK_API_VERIFY_URL || 'https://api.truvera.io/verify';
+export const DOCK_API_VERIFY_URL =
+  process.env.DOCK_API_VERIFY_URL || 'https://api.truvera.io/verify';
 export const DOCK_API_VERIFY_URL_TESTNET =
   process.env.DOCK_API_VERIFY_URL_TESTNET || 'https://api-testnet.truvera.io/verify';
 
